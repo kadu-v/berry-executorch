a91709badd05d7ef0685efa40994bc2168e3d0bd
kadu-v <kazuonsunday@yahoo.co.jp>
2024-10-08 10:04:32 +0900

    Add: doc comments

--- src/c_interface.rs
+++ src/c_interface.rs
@@ -8,9 +8,29 @@ pub struct CModule {
 }
 
 extern "C" {
+    ///
     pub fn c_new_module(file_path: *const c_char) -> *mut CModule;
+
+    /// Drop the executorch module.
+    ///
+    /// # Safety
+    /// ptr must be a valid pointer to a CModule.
     pub fn c_drop_module(ptr: *mut CModule);
+
+    /// Force load the executorch module.
+    ///
+    /// # Safety
+    /// ptr must be a valid pointer to a CModule.
     pub fn c_load(ptr: *mut CModule) -> i32;
+
+    /// Forward the executorch module.
+    ///
+    /// # Safety
+    /// ptr must be a valid pointer to a CModule.
+    /// input must be a valid pointer to a f32 array.
+    /// sizes must be a valid pointer to a f32 array and the length of the array must be equal to dim.
+    /// sizes represents the shape of the input tensor.
+    /// output must be a valid pointer to a f32 array and the output must be dropped by the caller.
     pub fn c_forward(
         ptr: *mut CModule,
         input: *const f32,
